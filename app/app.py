519dc431930530aa20d8731b4e785591f4683ff9
DRPatel75 <drpatel3009@gmail.com>
2026-05-18 13:32:01 +0530

    Written the Streamlit Code

---
+++ app/app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pandas as pd
+import joblib
+
+model = joblib.load(r"D:\Projects\ML PROJECT 1\models\best_model.joblib")
+
+st.title("Customer Churn Prediction")
+
+gender = st.selectbox("Gender", ["Male", "Female"])
+SeniorCitizen = st.selectbox("Senior Citizen", [0, 1])
+tenure = st.slider("Tenure", 0, 72)
+MonthlyCharges = st.number_input("Monthly Charges")
+
+if st.button("Predict"):
+
+    data = pd.DataFrame({
+        "gender": [gender],
+        "SeniorCitizen": [SeniorCitizen],
+        "tenure": [tenure],
+        "MonthlyCharges": [MonthlyCharges]
+    })
+
+    prediction = model.predict(date)
+
+    st.sucess(f"Prediction: {prediction[0]}")
